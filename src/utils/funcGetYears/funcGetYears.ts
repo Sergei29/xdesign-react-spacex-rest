3307a01637a6afbeed65e64275a09afaeb4c81ba
Sergetestcargo <protopulos.pulatikos@yandex.ru>
2021-03-06 13:18:37 +0000

    util function updated sort order

--- src/utils/funcGetYears/funcGetYears.ts
+++ src/utils/funcGetYears/funcGetYears.ts
@@ -1,14 +1,14 @@
 /**
  * @description util, aggregates the launch years from the list
  * @param {Array} arrLaunches launches list
- * @returns {Array} array of launched years
+ * @returns {Array} array of launched years, recent first
  */
 const funcGetYears = (arrLaunches: Record<string, any>[]): number[] => {
   const arrYearsWithDuplicates = arrLaunches.map((objLaunch) =>
     parseInt(objLaunch["launch_year"])
   );
 
-  return [...(new Set(arrYearsWithDuplicates) as any)];
+  return [...(new Set(arrYearsWithDuplicates) as any)].sort((a, b) => b - a);
 };
 
 export default funcGetYears;
